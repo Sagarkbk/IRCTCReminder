e4ed189b1c3b7ad787a7be51d7ccd2fd9b966115
Sagar <sagarkatnam@gmail.com>
2025-06-19 01:27:32 +0530

    Created new folder Telegram_Bot. And wrote basic bot functionality.

---
+++ Server/Telegram_Bot/bot.py
@@ -0,0 +1,67 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ContextTypes, CallbackQueryHandler, CommandHandler, Application
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        options = [
+            [InlineKeyboardButton("YES", callback_data="enable")],
+            [InlineKeyboardButton("NO", callback_data="do_not_enable")],
+        ]
+        await update.message.reply_text(
+            "Do you want to Enable Reminders?",
+            reply_markup=InlineKeyboardMarkup(options)
+        )
+        print("Inside start")
+    except Exception as e:
+        print(f"Exception in /start: {e}")
+
+async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        options = [
+            [InlineKeyboardButton("YES", callback_data="disable")],
+            [InlineKeyboardButton("NO", callback_data="do_not_disable")],
+        ]
+        await update.message.reply_text(
+            "Do you want to Disable Reminders?",
+            reply_markup=InlineKeyboardMarkup(options)
+        )
+        print("Inside stop")
+    except Exception as e:
+        print(f"Exception in /stop: {e}")
+
+async def commandHandler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        query = update.callback_query
+        if query:
+            await query.answer()
+            if query.data in ("enable", "do_not_enable"):
+                if query.data == "enable":
+                    await query.edit_message_text("✅ Reminders are Enabled! Use /stop to disable reminders")
+                elif query.data == "do_not_enable":
+                    await query.edit_message_text("❌ Reminders are not Enabled! Use /start to enable reminders")
+            if query.data in ("disable", "do_not_disable"):
+                if query.data == "disable":
+                    await query.edit_message_text("✅ Reminders are Disabled! Use /start to enable reminders")
+                elif query.data == "do_not_disable":
+                    await query.edit_message_text("❌ Reminders are not Disabled! Use /stop to disable reminders")
+        print("Inside commandHandler")
+    except Exception as e:
+        print(f"Exception in /commandHandler: {e}")
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("stop", stop))
+    app.add_handler(CallbackQueryHandler(commandHandler))
+    app.run_polling(
+        poll_interval=2.0, 
+        bootstrap_retries=3,)
+    print("Inside main")
+
+if __name__=="__main__":
+    main()
