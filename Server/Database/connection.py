ab91b7df3bc23ac5741a8c6cabfe613c3e1c5ad6
Sagar <sagarkatnam@gmail.com>
2025-06-23 01:47:40 +0530

    I'm going with pure asyncpg instead of sqlalchemy so that it will be more sql hands-on rather on ORM centric. Uninstalled sqlalchemy. Modified connection.py for connecting to Database using asyncpg and removed db logic from bot.py.

--- Server/Database/connection.py
+++ Server/Database/connection.py
@@ -1,21 +1,45 @@
-from sqlalchemy.ext.asyncio import create_async_engine
+import asyncpg
 import asyncio
 import os
-from .models import Base
 from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL=os.getenv("DATABASE_URL")
+class Database:
+    def __init__(self):
+        self.pool = None
+        self.DATABASE_URL = os.getenv("DATABASE_URL")
 
-async def test_connection():
+    async def connect(self):
+        if not self.DATABASE_URL:
+            raise ValueError("DATABASE_URL is not defined in Environment Variables")
+        self.pool = await asyncpg.create_pool(
+            self.DATABASE_URL,
+            min_size=2,
+            max_size=10            
+        )
+        print("Database connection pool created.")
+    
+    async def disconnect(self):
+        if self.pool:
+            await self.pool.close()
+            print("Database connection pool closed.")
+    
+    async def fetch_one(self, query, *args):
+        async with self.pool.acquire() as conn:
+            return await conn.fetchrow(query, *args)
+
+db = Database()
+
+async def test_conn_pool():
     try:
-        engine = create_async_engine(DATABASE_URL)
-        async with engine.begin() as conn:
-            await conn.run_sync(Base.metadata.create_all)
-        print("Tables created")
+        await db.connect()
+        result = await db.fetch_one('SELECT 1 AS test')
+        print(f"Test query result: {result['test']}")
+        print("Connected to database.")
     except Exception as e:
-        print(f"Exception : {e}")
+        print("Failed to connect to database. {e}")
+        return None
 
 if __name__=="__main__":
-    asyncio.run(test_connection())
+    asyncio.run(test_conn_pool())
--- Server/Telegram_Bot/bot.py
+++ Server/Telegram_Bot/bot.py
@@ -1,26 +1,11 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes, CallbackQueryHandler, CommandHandler, Application
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
-from Database.models import Users
 import os
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 DATABASE_URl=os.getenv("DATABASE_URL")
-
-engine = create_async_engine(DATABASE_URl)
-
-AsyncSessionLocal = sessionmaker(
-    bind=engine,
-    expire_on_commit=False,
-    class_=AsyncSession
-)
-
-async def get_db():
-    async with AsyncSessionLocal() as session:
-        return session
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
@@ -53,14 +38,10 @@ async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def commandHandler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         query = update.callback_query
-        session = await get_db()
         if query:
             await query.answer()
             if query.data in ("enable", "do_not_enable"):
                 if query.data == "enable":
-                    user = Users(telegram_id=update.effective_user.id)
-                    session.add(user)
-                    await session.commit()
                     await query.edit_message_text("✅ Reminders are Enabled! Use /stop to disable reminders")
                 elif query.data == "do_not_enable":
                     await query.edit_message_text("❌ Reminders are not Enabled! Use /start to enable reminders")
