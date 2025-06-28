02b34365751a96cbeb8814f39c1d04d3549d396d
Sagar <sagarkatnam@gmail.com>
2025-06-28 23:37:10 +0530

    Added HTTP Status Codes for exisiting endpoints.

--- Server/Routes/users.py
+++ Server/Routes/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Response, status
 from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime, timedelta
@@ -28,7 +28,7 @@ class LinkAccounts(BaseModel):
 usersRouter = APIRouter(prefix="/users")
 
 @usersRouter.get("/getUser/{google_id}")
-async def getUser(google_id: str):
+async def getUser(google_id: str, response: Response):
     try:
         await db.connect()
         get_user_query = """
@@ -36,19 +36,22 @@ async def getUser(google_id: str):
                 """
         existingUser = await db.fetchone(get_user_query, google_id)
         if not existingUser:
+            response.status_code = status.HTTP_404_NOT_FOUND
             return "User does not exist"
+        response.status_code = status.HTTP_200_OK
         return {"User Details" : existingUser}
     except Exception as e:
         print(f"Exception when hitting /getUser: {e}")
         raise
 
 @usersRouter.get("/getAllUsers")
-async def getAllUsers():
+async def getAllUsers(response: Response):
     try:
         await db.connect()
         get_users_query = """
                 SELECT * FROM users
                 """
+        response.status_code = status.HTTP_200_OK
         users = await db.fetchall(get_users_query)
         if not users:
             return "No users as of now"
@@ -58,7 +61,7 @@ async def getAllUsers():
         raise
 
 @usersRouter.post("/addUser")
-async def addUser(body: User):
+async def addUser(body: User, response: Response):
     try:
         await db.connect()
         get_user_with_googleid_query = """
@@ -66,12 +69,14 @@ async def addUser(body: User):
                 """
         existingUserWithId = await db.fetchone(get_user_with_googleid_query, body.google_id)
         if existingUserWithId:
+            response.status_code = status.HTTP_400_BAD_REQUEST
             return "User already exists with provided Google ID"
         get_user_with_email_query = """
                 SELECT * FROM users WHERE email = $1
                 """
         existingUserWithEmail = await db.fetchone(get_user_with_email_query, body.email)
         if existingUserWithEmail:
+            response.status_code = status.HTTP_400_BAD_REQUEST
             return "User already exists with provided Email"
         insert_user_query = """
                 INSERT INTO users (google_id, email, username, reminder_days, calendar_enabled) VALUES ($1, $2, $3, $4, $5)
@@ -81,13 +86,14 @@ async def addUser(body: User):
                 SELECT * FROM users WHERE google_id = $1
                 """
         newUser = await db.fetchone(get_user_query, body.google_id)
+        response.status_code = status.HTTP_201_CREATED
         return {"newUser" : newUser}
     except Exception as e:
         print(f"Exception when hitting /addUser: {e}")
         raise
 
 @usersRouter.post("/generateToken/{google_id}")
-async def generateToken(google_id):
+async def generateToken(google_id, response: Response):
     try:
         await db.connect()
         get_user_with_googleid_query = """
@@ -95,6 +101,7 @@ async def generateToken(google_id):
                 """
         existingUserWithId = await db.fetchone(get_user_with_googleid_query, google_id)
         if not existingUserWithId:
+            response.status_code = status.HTTP_400_BAD_REQUEST
             return "There is no user with provided Google ID"
         check_if_token_exists_query = """
                 SELECT * FROM google_telegram_link WHERE user_id = $1
@@ -103,6 +110,7 @@ async def generateToken(google_id):
         current_time = datetime.now(pytz.UTC)
         expires_at = current_time + timedelta(minutes=30)
         if unusedToken and (not unusedToken['is_used']) and (unusedToken['expires_at'] > current_time):
+            response.status_code = status.HTTP_200_OK
             return {"Unused/Non-Expired Token" : unusedToken['token']}
         if unusedToken and (unusedToken['is_used'] or (unusedToken['expires_at'] < current_time)):
             delete_expired_or_used_token_query = """
@@ -115,6 +123,7 @@ async def generateToken(google_id):
                 INSERT INTO google_telegram_link (user_id, token, expires_at) VALUES ($1, $2, $3)
                 """
         await db.execute(insert_token_query, existingUserWithId['id'], token, expires_at)
+        response.status_code = status.HTTP_201_CREATED
         return {"Token" : token}
 
     except Exception as e:
@@ -122,5 +131,5 @@ async def generateToken(google_id):
         raise
 
 @usersRouter.post("/linkAccounts")
-async def linkAccounts(body):
+async def linkAccounts(body: LinkAccounts, response: Response):
     pass
