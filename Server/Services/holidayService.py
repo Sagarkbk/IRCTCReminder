6e145b0aea31983d443d4e2e7bd52017d760a4b4
Sagar <sagarkatnam@gmail.com>
2025-07-10 00:16:28 +0530

    Removed check, update and insert queries and using only upsert. Made the query transactional.

--- Server/Services/holidayService.py
+++ Server/Services/holidayService.py
@@ -23,10 +23,10 @@ async def add_holidays(body):
                 """
         current_time = datetime.now(timezone.utc)
         holidays = []
-        for hn, hd, cat, dbs, rds in zip(body['holiday_name'], body['holiday_date'], 
-                                        body['category'], body['day_before_sent'], 
-                                        body['release_day_sent']):
-            holiday = await conn.execute(query, body['user_id'], hn, hd, cat, dbs, rds, current_time)
+        for hn, hd, cat, dbs, rds in zip(body.holiday_name, body.holiday_date, 
+                                            body.category, body.day_before_sent, 
+                                            body.release_day_sent):
+            holiday = await conn.execute(query, body.user_id, hn, hd, cat, dbs, rds, current_time)
             holidays.append(holiday)
         return holidays
     except Exception as e:
@@ -35,41 +35,26 @@ async def add_holidays(body):
 async def update_holidays(body):
     try:
         conn = await get_db_connection()
-        check_if_exists = """
-                SELECT * FROM selected_holidays WHERE user_id = $1 AND holiday_name = $2 AND holiday_date = $3
-                """
-        update_holiday = """
-                UPDATE selected_holidays SET day_before_sent = $1, release_day_sent = $2, last_updated_at = $3
-                WHERE user_id = $4 AND holiday_name = $5 AND holiday_date = $6 AND category = $7
-                RETURNING *
-                """
-        add_holiday = """
+        upsert_holiday = """
                 INSERT INTO selected_holidays
                 (user_id, holiday_name, holiday_date, category, day_before_sent, release_day_sent, last_updated_at)
                 VALUES ($1, $2, $3, $4, $5, $6, $7)
+                ON CONFLICT (user_id, holiday_name, holiday_date)
+                DO UPDATE
+                SET category = EXCLUDED.category,
+                day_before_sent = EXCLUDED.day_before_sent,
+                release_day_sent = EXCLUDED.release_day_sent,
+                last_updated_at = EXCLUDED.last_updated_at
                 RETURNING *
                 """
         current_time = datetime.now(timezone.utc)
         holidays = []
-        for hn, hd, cat, dbs, rds in zip(body['holiday_name'], body['holiday_date'], 
-                                        body['category'], body['day_before_sent'], 
-                                        body['release_day_sent']):
-            exists = await conn.fetch(check_if_exists, body.user_id, hn, hd)
-            if exists:
-                holiday = await conn.execute(
-                                            update_holiday,
-                                            dbs,
-                                            rds,
-                                            current_time,
-                                            body.user_id,
-                                            hn,
-                                            hd,
-                                            cat
-                                        )
-                holidays.append(holiday)
-            else:
-                holiday = await conn.execute(
-                                            add_holiday,
+        async with await conn.transaction() as trsc:
+            for hn, hd, cat, dbs, rds in zip(body.holiday_name, body.holiday_date, 
+                                            body.category, body.day_before_sent, 
+                                            body.release_day_sent):
+                holiday = await trsc.fetchrow(
+                                            upsert_holiday,
                                             body.user_id,
                                             hn,
                                             hd,
