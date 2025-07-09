47eb16fe26fbbfbe2cb46d954b0ca3c6ec01a129
Sagar <sagarkatnam@gmail.com>
2025-07-09 23:51:57 +0530

    Added code in holidayService.py

--- Server/Routes/holiday.py
+++ Server/Routes/holiday.py
@@ -11,15 +11,18 @@ async def getHolidays(body, response: Response):
         user_id = body['user_id']
         holidays = await get_existing_holidays(user_id)
         response.status_code = status.HTTP_200_OK
-        return {"message": "Here are your current holidays", "data": holidays}
+        return holidays
     except Exception as e:
         raise
 
 @holidayRouter.post("/add")
 async def addHolidays(body: HolidayBody, response: Response):
     try:
-        user_id = body['user_id']
-        holidays = await add_holidays(user_id)
+        if not(len(body['holiday_name'])==len(body['holiday_date'])==len(body['category'])==
+               len(body['day_before_sent'])==len(body['release_day_sent'])):
+            response.status_code = status.HTTP_400_BAD_REQUEST
+            return "Number of records is not matching"
+        holidays = await add_holidays(body)
         response.status_code = status.HTTP_200_OK
         return {"message": "Here are your holidays", "data": holidays}
     except Exception as e:
@@ -28,8 +31,11 @@ async def addHolidays(body: HolidayBody, response: Response):
 @holidayRouter.put("/update")
 async def updateHolidays(body: HolidayBody, response: Response):
     try:
-        user_id = body['user_id']
-        holidays = await update_holidays(user_id)
+        if not(len(body['holiday_name'])==len(body['holiday_date'])==len(body['category'])==
+               len(body['day_before_sent'])==len(body['release_day_sent'])):
+            response.status_code = status.HTTP_400_BAD_REQUEST
+            return "Number of records is not matching"
+        holidays = await update_holidays(body)
         response.status_code = status.HTTP_200_OK
         return {"message": "Here are your updated holidays", "data": holidays}
     except Exception as e:
--- Server/Services/holidayService.py
+++ Server/Services/holidayService.py
@@ -11,11 +11,74 @@ async def get_existing_holidays(user_id):
         return holidays
     except Exception as e:
         raise
-    finally:
-        await conn.close()
 
-async def add_holidays():
-    pass
+async def add_holidays(body):
+    try:
+        conn = await get_db_connection()
+        query = """
+                INSERT INTO selected_holidays
+                (user_id, holiday_name, holiday_date, category, day_before_sent, release_day_sent, last_updated_at)
+                VALUES ($1, $2, $3, $4, $5, $6, $7)
+                RETURNING *
+                """
+        current_time = datetime.now(timezone.utc)
+        holidays = []
+        for hn, hd, cat, dbs, rds in zip(body['holiday_name'], body['holiday_date'], 
+                                        body['category'], body['day_before_sent'], 
+                                        body['release_day_sent']):
+            holiday = await conn.execute(query, body['user_id'], hn, hd, cat, dbs, rds, current_time)
+            holidays.append(holiday)
+        return holidays
+    except Exception as e:
+        raise
 
-async def update_holidays():
-    pass
+async def update_holidays(body):
+    try:
+        conn = await get_db_connection()
+        check_if_exists = """
+                SELECT * FROM selected_holidays WHERE user_id = $1 AND holiday_name = $2 AND holiday_date = $3
+                """
+        update_holiday = """
+                UPDATE selected_holidays SET day_before_sent = $1, release_day_sent = $2, last_updated_at = $3
+                WHERE user_id = $4 AND holiday_name = $5 AND holiday_date = $6 AND category = $7
+                RETURNING *
+                """
+        add_holiday = """
+                INSERT INTO selected_holidays
+                (user_id, holiday_name, holiday_date, category, day_before_sent, release_day_sent, last_updated_at)
+                VALUES ($1, $2, $3, $4, $5, $6, $7)
+                RETURNING *
+                """
+        current_time = datetime.now(timezone.utc)
+        holidays = []
+        for hn, hd, cat, dbs, rds in zip(body['holiday_name'], body['holiday_date'], 
+                                        body['category'], body['day_before_sent'], 
+                                        body['release_day_sent']):
+            exists = await conn.fetch(check_if_exists, body.user_id, hn, hd)
+            if exists:
+                holiday = await conn.execute(
+                                            update_holiday,
+                                            dbs,
+                                            rds,
+                                            current_time,
+                                            body.user_id,
+                                            hn,
+                                            hd,
+                                            cat
+                                        )
+                holidays.append(holiday)
+            else:
+                holiday = await conn.execute(
+                                            add_holiday,
+                                            body.user_id,
+                                            hn,
+                                            hd,
+                                            cat,
+                                            dbs,
+                                            rds,
+                                            current_time
+                                        )
+                holidays.append(holiday)
+        return holidays
+    except Exception as e:
+        raise
