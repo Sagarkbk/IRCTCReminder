9ecee36b26957380ba600553f558a5039ac4dcbb
Sagar <sagarkatnam@gmail.com>
2025-07-30 22:41:08 +0530

    Installed tailwind and tested. Working as expected.

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-full h-full text-center flex flex-col gap-4 mt-4">
+      <div className="text-green-600 text-4xl">
+        Testing tailwind - 9xl Green Text
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="text-amber-900 text-3xl">
+        Testing tailwind - 7xl Amber Text
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
