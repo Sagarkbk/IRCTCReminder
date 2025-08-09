61d836a571232e8202808acfab97e6732c46958c
Sagar <sagarkatnam@gmail.com>
2025-08-09 13:31:21 +0530

    Using CORS middleware to allow frontend calls.

--- Server/main.py
+++ Server/main.py
@@ -13,6 +13,7 @@ import redis.asyncio as redis
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from Services.schedulerService import create_google_calendar_event, send_telegram_reminders
 from pendulum.tz.timezone import UTC
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
@@ -51,6 +52,19 @@ async def lifespan(app: FastAPI):
     print("Bot has been shutdown")
 
 app = FastAPI(lifespan=lifespan)
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(authRouter, prefix="/api")
 app.include_router(userRouter, prefix="/api")
