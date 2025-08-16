bfe6fc4549a41fa26c8216c71420b9fd1f301b16
Sagar <sagarkatnam@gmail.com>
2025-08-17 00:09:10 +0530

    Displaying Revoke/Connect text dynamically using calendar_enabled and telegram_enabled boolean values. And refactored AccountIntegration component by adding new component IntegrationCard since logic is repeating.

--- Client/src/components/AccountIntegrations.tsx
+++ Client/src/components/AccountIntegrations.tsx
@@ -1,4 +1,5 @@
 import { type User } from "../context/AuthContext";
+import { IntegrationCard } from "./IntegrationCard";
 
 export function AccountIntegrations({ user }: { user: User }) {
   if (!user) {
@@ -11,29 +12,14 @@ export function AccountIntegrations({ user }: { user: User }) {
         Integrations
       </h2>
 
-      <div className="bg-slate-800 rounded-lg p-6 shadow-lg mb-4">
-        <div className="flex items-center justify-between">
-          <div>
-            <p className="text-lg font-semibold">Google Calendar</p>
-            <p className="text-sm text-slate-400">Connected</p>
-          </div>
-          <button className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-lg transition-colors duration-200 w-36  cursor-pointer">
-            Revoke Access
-          </button>
-        </div>
-      </div>
-
-      <div className="bg-slate-800 rounded-lg p-6 shadow-lg">
-        <div className="flex items-center justify-between">
-          <div>
-            <p className="text-lg font-semibold">Telegram Bot</p>
-            <p className="text-sm text-slate-400">Not Connected</p>
-          </div>
-          <button className="bg-sky-600 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded-lg transition-colors duration-200 w-36 cursor-pointer">
-            Connect
-          </button>
-        </div>
-      </div>
+      <IntegrationCard
+        cardName={"Google Calendar"}
+        isEnabled={user.calendar_enabled}
+      />
+      <IntegrationCard
+        cardName={"Telegram"}
+        isEnabled={user.telegram_enabled}
+      />
     </section>
   );
 }
---
+++ Client/src/components/IntegrationCard.tsx
@@ -0,0 +1,29 @@
+export function IntegrationCard({
+  cardName,
+  isEnabled,
+}: {
+  cardName: string;
+  isEnabled: boolean;
+}) {
+  return (
+    <div className="bg-slate-800 rounded-lg p-6 shadow-lg mb-4">
+      <div className="flex items-center justify-between">
+        <div>
+          <p className="text-lg font-semibold">{cardName}</p>
+          <p className="text-sm text-slate-400">
+            {isEnabled ? "Connected" : "Not Connected"}
+          </p>
+        </div>
+        {isEnabled ? (
+          <button className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-lg transition-colors duration-200 w-36  cursor-pointer">
+            Revoke Access
+          </button>
+        ) : (
+          <button className="bg-sky-600 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded-lg transition-colors duration-200 w-36 cursor-pointer">
+            Connect
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
